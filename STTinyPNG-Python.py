88d53dd002492d59cf466ec8cddf71ee3cace45a
zpy <berniezhang428@gmail.com>
2016-05-06 15:37:31 +0800

    Update STTinyPNG-Python.py

--- STTinyPNG-Python.py
+++ STTinyPNG-Python.py
@@ -2,9 +2,9 @@ import tinify
 import os
 import os.path
 
-tinify.key = "your AppKey" # AppKey
-fromFilePath = "/Users/tangjr/Desktop/test1" # 源路径
-toFilePath = "/Users/tangjr/Desktop/test2" # 输出路径
+tinify.key = "RkoczqavMW1TjFrlhFE9WakzGCYmc2Rh" # AppKey
+fromFilePath = "/Users/zpy/Desktop/test1" # 源路径
+toFilePath = "/Users/zpy/Desktop/test2" # 输出路径
 
 for root, dirs, files in os.walk(fromFilePath):
 	for name in files:
