05438b229c2179fd2626c90651a6eb633e853507
zpy <zpy@98ki.com>
2016-05-06 15:56:16 +0800

    添加python 脚本

--- 98tinyPNG.py
+++ 98tinyPNG.py
@@ -1,10 +1,10 @@
-import tinify
 import os
 import os.path
+import tinify
 
-tinify.key = "RkoczqavMW1TjFrlhFE9WakzGCYmc2Rh" # AppKey
-fromFilePath = "/Users/zpy/Desktop/test1" # 源路径
-toFilePath = "/Users/zpy/Desktop/test2" # 输出路径
+tinify.key = "RkoczqavMW1TjFrlhFE9WakzGCYmc2Rh"
+fromFilePath = "/Users/zpy/Desktop/test1" 
+toFilePath = "/Users/zpy/Desktop/test2" 
 
 for root, dirs, files in os.walk(fromFilePath):
 	for name in files:
